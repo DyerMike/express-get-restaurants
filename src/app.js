6d8a683afccd61350cbc0952ab2725b9806befb2
dyer-mike <michael.dyer1@barclays.com>
2024-05-20 16:32:53 +0100

    completed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (request, response) =>{
+    const restaurant = await Restaurant.findAll();
+    response.json(restaurant);
+})
 
 
 
